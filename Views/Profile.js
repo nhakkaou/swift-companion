1f4a84de6bfcde5f8f2ff6170e444e99b9504be8
nhakkaou <noureddine.hak@gmail.com>
2021-10-09 02:39:05 +0100

    Fix Style Profile

--- Views/Profile.js
+++ Views/Profile.js
@@ -7,12 +7,15 @@ import {
   SafeAreaView,
   Image,
   ImageBackground,
+  ProgressBarAndroid,
+  ScrollView,
 } from "react-native";
-import { ProgressBar } from "react-native-paper";
-import { Avatar } from "react-native-paper";
 
+import { Avatar } from "react-native-paper";
+import result from "./test.json";
+import coalition from "./coallition.json";
 const Flex = ({ route }) => {
-  const { result, coalition } = route.params;
+  // const { result, coalition } = route.params;
   function coalition_style(bgcolor = "none") {
     return {
       backgroundColor: bgcolor,
@@ -44,144 +47,185 @@ const Flex = ({ route }) => {
       textAlign: "center",
     };
   }
-  console.log(coalition[0]?.color);
+  console.log(coalition.image_url);
   const image = {
     uri: `${result?.image_url}`,
   };
   const cover = {
-    uri: `${coalition[0]?.cover_url}`,
+    uri: `${coalition?.cover_url}`,
   };
   const img_coallition = {
-    uri: `${coalition[0]?.image_url}`,
+    uri: `${coalition.image_url}`,
   };
   return (
     <SafeAreaView>
-      <View
-        style={[
-          styles.container,
-          {
-            flexDirection: "column",
-            flex: 1,
-          },
-        ]}
-      >
-        {result ? (
-          <>
-            <StatusBar
-              animated={true}
-              backgroundColor={coalition[0]?.color}
-              hidden={false}
-            />
-            <ImageBackground
-              source={cover}
-              resizeMode="cover"
-              style={styles.image}
-            >
-              <View style={{ flexDirection: "row", flexWrap: "wrap", top: 0 }}>
-                <View>
-                  <View style={coalition_style(coalition[0]?.color)}>
+      <ScrollView>
+        <View
+          style={[
+            styles.container,
+            {
+              flexDirection: "column",
+              flex: 1,
+              // position: "relative",
+            },
+          ]}
+        >
+          {result ? (
+            <>
+              <StatusBar
+                animated={true}
+                backgroundColor={coalition?.color}
+                hidden={false}
+              />
+
+              <ImageBackground
+                source={cover}
+                resizeMode="cover"
+                style={styles.image}
+              >
+                <View
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 20,
+                  }}
+                >
+                  <View style={coalition_style(coalition?.color)}>
                     <Image
                       source={img_coallition}
                       style={styles.img_coallition}
                     />
                   </View>
-                  <Text style={titleText(coalition[0]?.color, "500", 14)}>
-                    {coalition[0]?.name}
+                  <Text style={titleText(coalition?.color, "500", 14)}>
+                    {coalition?.name}
                   </Text>
                 </View>
-                <Avatar.Image
-                  size={150}
-                  source={{ uri: `${result.image_url}` }}
-                  style={styles.img}
-                />
-              </View>
-            </ImageBackground>
-            {/* <View style={{ padding: 20 }}>
-              <Text style={titleText(coalition[0]?.color)}>
-                {result?.login}
-              </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    alignContent: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Avatar.Image
+                    size={150}
+                    source={{ uri: `${result.image_url}` }}
+                    style={styles.img}
+                  />
+                </View>
+              </ImageBackground>
+              <Text style={titleText(coalition?.color)}>{result?.login}</Text>
               <Text style={styles.nameStyle}>
                 {result?.first_name} {result?.last_name}
               </Text>
+
               <View
                 style={{
+                  padding: 20,
                   flexDirection: "row",
                   flexWrap: "wrap",
                   justifyContent: "space-between",
+                  alignItems: "stretch",
                 }}
               >
-                <Text style={inStyle(coalition[0]?.color)}>Grade</Text>
+                <Text style={inStyle(coalition?.color)}>Grade</Text>
                 <Text style={styles.textStyle}>
                   {result.cursus_users[0].grade}
                 </Text>
               </View>
               <View
                 style={{
+                  padding: 20,
                   flexDirection: "row",
                   flexWrap: "wrap",
                   justifyContent: "space-between",
+                  alignItems: "stretch",
                 }}
               >
-                <Text style={inStyle(coalition[0].color)}>Email</Text>
+                <Text style={inStyle(coalition.color)}>Email</Text>
                 <Text style={styles.textStyle}>{result.email}</Text>
               </View>
               <View
                 style={{
+                  padding: 20,
                   flexDirection: "row",
                   flexWrap: "wrap",
                   justifyContent: "space-between",
+                  alignItems: "stretch",
                 }}
               >
-                <Text style={inStyle(coalition[0]?.color)}>Phone</Text>
+                <Text style={inStyle(coalition?.color)}>Phone</Text>
                 <Text style={styles.textStyle}>{result?.phone}</Text>
               </View>
               <View
                 style={{
+                  padding: 20,
                   flexDirection: "row",
                   flexWrap: "wrap",
                   justifyContent: "space-between",
+                  alignItems: "stretch",
                 }}
               >
-                <Text style={inStyle(coalition[0].color)}>Wallet</Text>
+                <Text style={inStyle(coalition.color)}>Wallet</Text>
                 <Text style={styles.textStyle}>{result?.wallet}</Text>
               </View>
               <View
                 style={{
+                  padding: 20,
                   flexDirection: "row",
                   flexWrap: "wrap",
                   justifyContent: "space-between",
+                  alignItems: "stretch",
                 }}
               >
-                <Text style={inStyle(coalition[0].color)}>
-                  Correction point
-                </Text>
+                <Text style={inStyle(coalition.color)}>Correction point</Text>
                 <Text style={styles.textStyle}>{result.correction_point}</Text>
               </View>
               <View
                 style={{
+                  padding: 20,
                   flexDirection: "row",
                   flexWrap: "wrap",
                   justifyContent: "space-between",
+                  alignItems: "stretch",
                 }}
               >
-                <Text style={inStyle(coalition[0].color)}>Level</Text>
+                <Text style={inStyle(coalition.color)}>Level</Text>
                 <Text style={styles.textStyle}>
                   {result.cursus_users[0].level}
                 </Text>
               </View>
-            </View> */}
-            {/* <ProgressBar
-              progress={
-                "0" + result.cursus_users[0].level.toString().match(/.\d+/g)
-              }
-              color={`${coalition[0].color}`}
-              style={{ height: 20, backgroundColor: "white" }}
-            ></ProgressBar> */}
-          </>
-        ) : (
-          ""
-        )}
-      </View>
+
+              <View
+                style={{ width: "100%", padding: 20, alignItems: "center" }}
+              >
+                {result.cursus_users[0].skills.map((el, key) => {
+                  return (
+                    <>
+                      <Text
+                        key={key}
+                        style={titleText("#ffffff", "normal", 15)}
+                      >
+                        {el.name}
+                      </Text>
+                      <ProgressBarAndroid
+                        styleAttr="Horizontal"
+                        indeterminate={false}
+                        // progress={`0${el.level.toString().match(/.\d+/g)}`}
+                        progress={0.7}
+                        color={coalition.color}
+                      />
+                    </>
+                  );
+                })}
+              </View>
+            </>
+          ) : (
+            ""
+          )}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -192,8 +236,17 @@ const styles = StyleSheet.create({
     // padding: 20,
   },
   image: {
-    flex: 1,
+    // flex: 1,
     justifyContent: "center",
+    width: "100%",
+    height: 250,
+  },
+  img: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    // marginTop: 10,
+    // marginBottom: 10
   },
   nameStyle: {
     color: "white",
@@ -207,18 +260,9 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     fontSize: 14,
   },
-  img: {
-    width: 150,
-    height: 150,
-    borderRadius: 100,
-    margin: "auto",
-    marginTop: 10,
-    marginBottom: 10,
-  },
   img_coallition: {
     width: 50,
     height: 50,
-    margin: "auto",
   },
 });
 
