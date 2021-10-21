18e715c698eda835454bf7415518d42320939c3a
nhakkaou <noureddine.hak@gmail.com>
2021-10-21 19:51:55 +0100

    Update

--- App.js
+++ App.js
@@ -2,9 +2,7 @@ import * as React from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { ProfileScreen, ProjectScreen, HomeScreen } from "./Views";
-import axios from "axios";
 import { Icon } from "react-native-elements";
-import { AsyncStorage } from "react-native";
 
 const darkTheme = {
   dark: true,
@@ -17,9 +15,7 @@ const darkTheme = {
     notification: "rgb(255, 69, 58)",
   },
 };
-const uid = "8b3f2aa1e1b4e9ea2ed2399b3783dba79813b948c9ecbb9555f7927a7b530e0a";
-const client =
-  "fdf4044597bc43b22cf442e825051dda952c834d1d16d8aebfdb6c223df3a37d";
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
@@ -29,7 +25,7 @@ export default function App() {
       set={setRes}
       theme={dark}
       setTheme={setDark}
-      token={token}
+      // token={token}
     />
   );
 
@@ -45,53 +41,8 @@ export default function App() {
     />
   );
   const [result, setRes] = React.useState([]);
-  const [token, setToken] = React.useState("");
+  // const [token, setToken] = React.useState("");
   const [dark, setDark] = React.useState(true);
-  React.useEffect(() => {
-    try {
-      async function GetToken() {
-        console.log("test");
-        const token = await AsyncStorage.getItem("TOKEN");
-        const date = await AsyncStorage.getItem("DATE");
-        console.log(token, date);
-        if (!token || date || date > Date.now()) {
-          console.log("HIII");
-          axios
-            .post(
-              "https://api.intra.42.fr/oauth/token",
-              {
-                grant_type: "client_credentials",
-                client_id: uid,
-                client_secret: client,
-              },
-              { timeout: 2000 }
-            )
-            .then(async (tk) => {
-              setToken(tk.data.access_token);
-              await AsyncStorage.setItem("TOKEN", tk.data.access_token);
-              await AsyncStorage.setItem(
-                "DATE",
-                new Date(tk.data.created_at * 1000 + tk.data.expires_in * 1000)
-              );
-            });
-        } else setToken(token);
-      }
-      GetToken();
-    } catch (er) {
-      console.log(er);
-    }
-  }, []);
-  // setInterval(() => {
-  //   alert("Expire");
-  //   axios
-  //     .post("https://api.intra.42.fr/oauth/token", {
-  //       grant_type: "client_credentials",
-  //       client_id: uid,
-  //       client_secret: client,
-  //     })
-  //     .then((tk) => setToken(tk?.data.access_token));
-  // }, delay);
-
   return (
     <NavigationContainer
       theme={dark ? darkTheme : DefaultTheme}
--- Views/Home.js
+++ Views/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   TextInput,
@@ -10,17 +10,55 @@ import {
   TouchableOpacity,
   SafeAreaView,
 } from "react-native";
+import { AsyncStorage } from "react-native";
 import axios from "axios";
 
-const Home = ({ navigation, set, theme, setTheme, token }) => {
+const Home = ({ navigation, set, theme, setTheme }) => {
   const [text, setvalue] = useState("");
-  const [isEnabled, setIsEnabled] = useState(false);
   const [load, setLoad] = useState(false);
-  const toggleSwitch = () => {
-    setIsEnabled(!isEnabled);
-    setTheme(!theme);
-  };
-
+  const [token, setToken] = React.useState("");
+  const uid =
+    "8b3f2aa1e1b4e9ea2ed2399b3783dba79813b948c9ecbb9555f7927a7b530e0a";
+  const client =
+    "fdf4044597bc43b22cf442e825051dda952c834d1d16d8aebfdb6c223df3a37d";
+  React.useEffect(() => {
+    try {
+      async function GetToken() {
+        console.log("test");
+        const token = await AsyncStorage.getItem("TOKEN");
+        const date = await AsyncStorage.getItem("DATE");
+        console.log(token, date);
+        if (!token || !date || date > Date.now()) {
+          console.log("HIII");
+          axios
+            .post(
+              "https://api.intra.42.fr/oauth/token",
+              {
+                grant_type: "client_credentials",
+                client_id: uid,
+                client_secret: client,
+              },
+              { timeout: 2000 }
+            )
+            .then(async (tk) => {
+              setToken(tk.data.access_token);
+              console.log(tk.data);
+              console.log(
+                new Date(tk.data.created_at * 1000 + tk.data.expires_in * 1000)
+              );
+              await AsyncStorage.setItem("TOKEN", tk.data.access_token);
+              await AsyncStorage.setItem(
+                "DATE",
+                tk.data.created_at + tk.data.expires_in * 1000
+              );
+            });
+        } else setToken(token);
+      }
+      GetToken();
+    } catch (er) {
+      console.log(er);
+    }
+  }, []);
   const ft_search = () => {
     setLoad(true);
     if (text)
@@ -51,31 +89,49 @@ const Home = ({ navigation, set, theme, setTheme, token }) => {
           alert("User Dosnt existe");
         });
   };
-
   return (
     <SafeAreaView
-      style={{ backgroundColor: isEnabled ? "#fff" : "#000", flex: 1 }}
+      style={{ backgroundColor: !theme ? "#FEF5ED" : "#000", flex: 1 }}
     >
       <View style={styles.Switch}>
         <Switch
           trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          thumbColor={theme ? "#f5dd4b" : "#f4f3f4"}
           ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
+          onValueChange={() => setTheme(!theme)}
+          value={!theme}
         />
       </View>
       <View style={styles.sousContainer}>
-        <Image style={styles.logo} source={require("../assets/1337.png")} />
+        <Image
+          style={styles.logo}
+          source={
+            theme
+              ? require("../assets/1337.png")
+              : require("../assets/1337-black.png")
+          }
+        />
         {token && !load ? (
           <View style={styles.login_container}>
             <TextInput
               placeholder="Login.."
-              style={styles.input}
+              style={[
+                styles.input,
+                {
+                  backgroundColor: theme ? "white" : "#D3E4CD",
+                  color: theme ? "#292D39" : "#000",
+                },
+              ]}
               onChangeText={(e) => setvalue(e.trim())}
               value={text}
             />
-            <TouchableOpacity onPress={ft_search} style={styles.button}>
+            <TouchableOpacity
+              onPress={ft_search}
+              style={[
+                styles.button,
+                { backgroundColor: theme ? "#292D39" : "#99A799" },
+              ]}
+            >
               <Text style={{ color: "#ffffff" }}> Search </Text>
             </TouchableOpacity>
           </View>
@@ -92,22 +148,18 @@ const styles = StyleSheet.create({
   },
   sousContainer: {
     justifyContent: "center",
-    // margin: "10",
     alignItems: "center",
   },
   input: {
     width: 250,
     height: 30,
-    backgroundColor: "white",
     borderRadius: 1.5,
-    color: "#292D39",
   },
   logo: {
     width: 200,
     height: 200,
   },
   button: {
-    backgroundColor: "#292D39",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 1.5,
